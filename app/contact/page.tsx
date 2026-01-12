7b0ee318a4652feb77d7dcafc4d4c251868fe167
Vesper Solutions <78233861+vespersolutionspk18@users.noreply.github.com>
2026-01-12 14:51:35 +0500

    t1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,13 +13,7 @@ const page = () => {
         <div className="w-full h-full bg-gray-950 rounded-2xl flex flex-col p-20">
             <h3 className="text-5xl text-white font-light">Visit us or drop a call.</h3>
             <h5 className="text-2xl text-white mt-5 mb-1 font-thin">Head Office</h5>
-            <p className="text-white font-thin">Office No. 36, The Pearl Continental, Mall Road, Lahore</p>
-            <h5 className="text-2xl text-white mt-3 mb-1 font-thin">Islamabad Office</h5>
-            <p className="text-white font-thin">Suite No. 202, One Constitution Avenue, Islamabad</p>
-            <h5 className="text-2xl text-white mt-3 mb-1 font-thin">Karachi Office</h5>
-            <p className="text-white font-thin">House No 67, Khayban-e-Badban, Phase V, Karachi</p>
-            <h5 className="text-2xl text-white mt-3 mb-1 font-thin">Peshawar Office</h5>
-            <p className="text-white font-thin">C2, 2nd Floor, New Park View Apartments, Peshawar</p>
+            <p className="text-white font-thin">514, 5th Floor, Gold Crest Mall, Phase IV, DHA, Lahore, Pakistan</p>
             <h5 className="text-2xl text-white mt-3 mb-1 font-thin">Phone</h5>
             <p className="text-white font-thin">+92 300 5557859</p>
             <h5 className="text-2xl text-white mt-3 mb-1 font-thin">Email</h5>
